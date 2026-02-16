162be9d3b7802abbaa892b330960ae5c60f48c5e
Locomaster <wrasse219@gmail.com>
2026-02-16 13:23:14 -0800

    Camera Movement and Early Background
    Added some basic camera movement controls (WASD) and a basic background to view when moving the camera with some "targets" (For later development)

---
+++ Assets/scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+}
